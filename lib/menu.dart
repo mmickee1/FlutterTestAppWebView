c80bda09087bde2e23afb2da75f18906f6dc71c6
Mikael Meinander <mikael.meinander@qvik.fi>
2023-12-04 16:27:03 +0200

    javascript channels

--- lib/menu.dart
+++ lib/menu.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
-enum _MenuOptions {
-  navigationDelegate, userAgent
-}
+enum _MenuOptions { navigationDelegate, userAgent, javascriptChannel }
 
 class Menu extends StatefulWidget {
   const Menu({required this.controller, super.key});
@@ -12,17 +10,17 @@ class Menu extends StatefulWidget {
 
   @override
   State<Menu> createState() => _MenuState();
-
 }
 
-  class _MenuState extends State<Menu> {
+class _MenuState extends State<Menu> {
   @override
   Widget build(BuildContext context) {
     return PopupMenuButton<_MenuOptions>(
       onSelected: (value) async {
         switch (value) {
           case _MenuOptions.navigationDelegate:
-            await widget.controller.loadRequest(Uri.parse('https://youtube.com'));
+            await widget.controller
+                .loadRequest(Uri.parse('https://youtube.com'));
             break;
           case _MenuOptions.userAgent:
             final userAgent = await widget.controller
@@ -31,6 +29,21 @@ class Menu extends StatefulWidget {
             ScaffoldMessenger.of(context).showSnackBar(SnackBar(
               content: Text('$userAgent'),
             ));
+            break;
+          case _MenuOptions.javascriptChannel:
+            await widget.controller.runJavaScript('''
+var req = new XMLHttpRequest();
+req.open('GET', "https://api.ipify.org/?format=json");
+req.onload = function() {
+  if (req.status == 200) {
+    let response = JSON.parse(req.responseText);
+    SnackBar.postMessage("IP Address: " + response.ip);
+  } else {
+    SnackBar.postMessage("Error: " + req.status);
+  }
+}
+req.send();''');
+            break;
         }
       },
       itemBuilder: (context) => [
@@ -41,6 +54,10 @@ class Menu extends StatefulWidget {
         const PopupMenuItem<_MenuOptions>(
           value: _MenuOptions.userAgent,
           child: Text('Show user-agent'),
+        ),
+        const PopupMenuItem<_MenuOptions>(
+          value: _MenuOptions.javascriptChannel,
+          child: Text('Lookup IP Address'),
         ),
       ],
     );
--- lib/web_view_stack.dart
+++ lib/web_view_stack.dart
@@ -50,7 +50,14 @@ class _WebViewStackState extends State<WebViewStack> {
           },
         ),
       )
-      ..setJavaScriptMode(JavaScriptMode.unrestricted);
+      ..setJavaScriptMode(JavaScriptMode.unrestricted)
+      ..addJavaScriptChannel(
+        'SnackBar',
+        onMessageReceived: (message) {
+          ScaffoldMessenger.of(context)
+              .showSnackBar(SnackBar(content: Text(message.message)));
+        },
+      );
   }
 
   @override
